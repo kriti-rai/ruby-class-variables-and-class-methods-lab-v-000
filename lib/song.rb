e8c9a727e26aca935c857a85e3940800c8b2ad28
Kriti Rai <kriti.rai@gmail.com>
2018-01-31 22:57:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,13 @@
 class Song
-  count
-  artists
-  genres
-  genre_count
-  artist_count
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+  # count
+  # artists
+  # genres
+  # genre_count
+  # artist_count
 end
