0fbde99581bc8b7c27eb53be8836d904c6eac464
Kriti Rai <kriti.rai@gmail.com>
2018-02-01 00:27:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,19 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@genres = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count +=1
+    @@genres << genre
   end
 
   def self.count
     @@count
   end
-  
+
   def self.genres
     #returns array of all genres of existing songs
   end
