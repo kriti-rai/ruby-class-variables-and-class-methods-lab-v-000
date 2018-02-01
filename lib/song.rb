81aaa8a0512b38e1bb4f20b474989e5f2b94a768
Kriti Rai <kriti.rai@gmail.com>
2018-02-01 01:23:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,5 +40,13 @@ class Song
   end
 
   def self.artist_count
+    @@artists.each do |artist|
+      if @@artist_count.key?(artist)
+        @@artist_count[artist] += 1
+      else
+        @@artist_count[artist] = 1
+      end
+    end
+    @@artist_count
   end
 end
